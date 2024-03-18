19336a5342c4ad1377fbdae40938dbb1dfa62252
nada <nadayahya2020@hotmail.com>
2024-03-18 06:34:11 -0700

    Done Formula Temp

---
+++ src/FormulaTemp.cs
@@ -0,0 +1,36 @@
+class FormulaTemp{
+    public static string F_to_C_Formula(double F)
+    {
+        return $"({F} - 32) * 5 / 9";
+    }
+
+    public static string C_to_F_Formula(double C)
+    {
+        return $"({C} * 9 / 5) + 32";
+    }
+
+    public static string C_to_K_Formula(double C)
+    {
+        return $"{C} + 273.15";
+    }
+
+    public static string K_to_C_Formula(double K)
+    {
+        return $"{K} - 273.15";
+    }
+    public static string K_to_F_Formula(double K)
+    {
+        var celsius = ConvertTemp.K_to_C(K);
+        var celsiusFormula =K_to_C_Formula(K);
+        var fahrenheit = C_to_F_Formula(celsius);
+        return celsiusFormula + fahrenheit ;
+    }
+
+    public static string F_to_K_Formula(double F)
+    {
+        var celsius = ConvertTemp.F_to_C(F);
+        var celsiusFormula = F_to_C_Formula(F);
+        var kelvin = C_to_K_Formula(celsius);
+        return celsiusFormula + kelvin;
+    }
+}
